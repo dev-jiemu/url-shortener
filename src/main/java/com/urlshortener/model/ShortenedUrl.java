fe6dfa0dbfd8d1d7ecc08c0e13f94d1a41acdb5f
jiemu <rebianne03@naver.com>
2025-05-26 14:33:12 +0900

    2025.05.26 init project - gradle update (rombok)

--- src/main/java/com/urlshortener/model/ShortenedUrl.java
+++ src/main/java/com/urlshortener/model/ShortenedUrl.java
@@ -3,7 +3,6 @@ package com.urlshortener.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.cglib.core.Local;
 
 import java.time.LocalDateTime;
 
